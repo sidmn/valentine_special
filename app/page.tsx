f4f91c0214dca4e81483d7704909fe26a933c428
sidmn <sidmantri3@gmail.com>
2025-02-13 00:37:57 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,82 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import catgif1 from "./assets/catgif1.gif"
+import catgif2 from "./assets/catgif2.gif"
+import catgif3 from "./assets/catgif3.gif"
+import catgif4 from "./assets/catgif4.gif"
+import catgif5 from "./assets/catgif5.gif"
+import catgif6 from "./assets/catgif6.gif"
+import catgif7 from "./assets/catgif7.gif"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const prompts = [
+    "Will you go out with me?",
+    "Are you sure you dont want to go out????",
+    "Are you really really suree???",
+    "Please say yesssss > <",
+    "I won't dissapoint you, please be my valentine? final offer",
+    "Did you really think I would let you say No???",
+    "Lets Goo!!! My kudi said yesssss!!!"
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const gifs = [
+    catgif1,
+    catgif2,
+    catgif3,
+    catgif4,
+    catgif5,
+    catgif6,
+    catgif7
+  ]
+
+  const [count, setCount] = useState(0);
+  const [no, setNo] = useState("No");
+  const [yes,setYes] = useState("hidden")
+  
+
+  function handleYes() {
+    setYes("block")
+    setCount(6)
+  }
+
+  function handleNo() {
+    if (count < 4) setCount(count + 1);
+    else if(count==4){
+      setCount(count + 1);
+      setNo("Yes");
+    }
+    else {
+
+      handleYes();
+    }
+  }
+
+  return (
+    <main className=" flex flex-col items-center justify-center h-screen gap-5" >
+      <Image
+        src={gifs[count]}
+        alt="funny gif"
+        width={200}
+        height={200}
+        unoptimized={true}
+      />
+      <div className="pb-5">{prompts[count]}</div>
+      <div className={yes}>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/C8nxICQI5Xo?si=r5307keoExVmq5rJ?autoplay=1" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+      </div>
+      <div className="flex flex-row gap-10">
+        <button className="border text-white bg-[#33b249] rounded-lg py-1 px-4" onClick={handleYes}>
+          Yes
+        </button>
+        <button
+          className="border bg-red-600 text-white rounded-lg py-1 px-4"
+          onClick={handleNo}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          {no}
+        </button>
+      </div>
+    </main>
   );
 }
